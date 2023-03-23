309b9d2bc570f56ce417337bfd5e1e03ea08c12d
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-23 18:11:41 +0100

    edited js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,23 @@ import Notiflix from 'notiflix';
 //.then(res => {
 //  console.log(res.data);
 //});
+const wordInput = document.querySelector('.search-form');
+
+const searchParams = new URLSearchParams({
+  key: '34647684-abfdb43770480e65049b2781c',
+  q: wordInput,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+});
+
+const IMAGES_URL = 'https://pixabay.com/api/?${searchParams}';
+
+const fetchImages = async () => {
+  const response = await axios.get(IMAGES_URL);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  const images = await response.json();
+  return images;
+};
