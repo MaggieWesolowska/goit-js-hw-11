6a14a1687e73a5871e51ab38b86dcbae02e2549b
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-23 18:35:56 +0100

    edited url link

--- src/index.js
+++ src/index.js
@@ -1,35 +1,21 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-//axios.get('/users')
-//.then(res => {
-//  console.log(res.data);
-//});
 const wordInput = document.querySelector('.search-form');
 const API_KEY = '34647684-abfdb43770480e65049b2781c';
 
 const searchParams = new URLSearchParams({
-  key: API_KEY,
   q: wordInput,
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
 });
 
-const responseParams = {
-  webformatURL,
-  largeImageURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-};
-
 const IMAGES_URL =
-  'https://pixabay.com/api/?${searchParams}' + '${responseParams}';
+  'https://pixabay.com/api/?key=' + API_KEY + '${searchParams}';
 
-const fetchImages = async () => {
+const fetchImages = async q => {
+  const token = await axios.get('https://pixabay.com/api/?key=' + API_KEY);
   const response = await axios.get(IMAGES_URL);
   if (!response.ok) {
     throw new Error(response.statusText);
